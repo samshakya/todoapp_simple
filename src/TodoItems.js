7af5f03be937e44860d0aa093f328404a87774e1
Samrat Shakya <shakyasamrat@192-168-1-101.tpgi.com.au>
2020-02-17 15:34:03 +1100

    First Commit

---
+++ src/TodoItems.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+
+    this.createTask = this.createTask.bind(this);
+  }
+  createTask(item) {
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}  
+      </li>
+    );
+  }
+  delete(key) {
+    this.props.delete(key);
+  }
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTask);
+
+    return <ul className="theList">{listItems}</ul>;
+  }
+}
+export default TodoItems;
